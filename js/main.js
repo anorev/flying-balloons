9267e6ecdd117200d96148dd353531fcd7831fb3
Robin Verona <robin.verona@edu.gobelins.frls>
2021-12-14 13:29:07 +0100

    update js to move each balloon differently

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-const balloon = document.querySelectorAll('.balloon')
-const randomX = random(10, 20);
-const randomY = random(20, 30);
+/**
+ * VARS
+ */
+
+const balloon1 = document.querySelector('#balloonSvg1')
+const balloon2 = document.querySelector('#balloonSvg2')
+const balloon3 = document.querySelector('#balloonSvg3')
+
+const randomX = random(1, 3);
+const randomY = random(1, 5);
 const randomDelay = random(0, 1);
-const randomTime = random(3, 5);
+const randomTime = random(1, 5);
 const randomTime2 = random(5, 10);
 const randomAngle = random(8, 12);
 
-TweenLite.set(balloon, {
-  x: randomX(-1),
-  y: randomX(1),
-  rotation: randomAngle(-1)
-});
 
-moveX(balloon, 1);
-moveY(balloon, -1);
-rotate(balloon, 1);
+
+/** FUNCTIONS */
 
 function rotate(target, direction) {
   
@@ -52,6 +53,47 @@ function random(min, max) {
   return (direction = 1) => (min + delta * Math.random()) * direction;
 }
 
+
+
+/**
+ * Functions calls
+ */
+TweenLite.set(balloon1, {
+  x: randomX(-4),
+  y: randomY(1),
+  rotation: randomAngle(-1)
+});
+
+moveX(balloon1, 1);
+moveY(balloon1, -1);
+rotate(balloon1, 1);
+
+
+TweenLite.set(balloon2, {
+  x: randomX(-0.8),
+  y: randomX(0.9),
+  rotation: randomAngle(2)
+});
+
+moveX(balloon2, 1);
+moveY(balloon2, -1);
+rotate(balloon2, 1);
+
+TweenLite.set(balloon3, {
+  x: randomX(-1.4),
+  y: randomX(2),
+  rotation: randomAngle(-3)
+});
+
+moveX(balloon3, 1);
+moveY(balloon3, -1);
+rotate(balloon3, 1);
+
+/**
+ * Others anims
+ */
+
+
 gsap.from(
     'body', {
         backgroundColor: 'black',
@@ -68,3 +110,11 @@ gsap.from(
         ease: Elastic. easeOut.config( 1, 1.2)
     }
 )
+
+
+
+
+
+
+
+
